fd93891318fe584f5cef87ebe055684cff936318
David Olabode <davvido51@gmail.com>
2020-06-07 17:42:01 +0100

    Handled error on send-push

--- src/helper/send-push/actions/send-push.ts
+++ src/helper/send-push/actions/send-push.ts
@@ -2,10 +2,18 @@ import { Request, Response, NextFunction } from 'express'
 import triggerPush from './trigger-push'
 
 const sendpush = async (req: Request, res: Response, next: NextFunction) => {
-    triggerPush(req.body.subscriptions, req.body.payload);
-    res.status(200).json({
-        status: 'success'
-    })
+    try {
+        triggerPush(req.body.subscriptions, req.body.payload);
+        res.status(200).json({
+            status: 'success'
+        })
+    } catch (err) {
+        res.status(err.statusCode).json({
+            status: 'fail',
+            message: err.message
+        })
+    }
+
 }
 
 export default sendpush
